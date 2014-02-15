f0768f8dd4d5578791d0c008c23490317777eb97
Marios Sotiropoulos <mai1211@uom.edu.gr>
2014-02-15 16:43:10 +0200

    Initialize the repository.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MsOAuthRuby::Application.config.secret_key_base = '7c8f9d3952c378798275adeb7e2085fbdf83eed311c38ab5dd6925e92a4853f8ffecf2c41e12a150aca8d1e29538b9f5b4ed87a84a98e57382168a29d6805414'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MsOAuthRuby::Application.config.session_store :cookie_store, key: '_msOAuth-Ruby_session'
